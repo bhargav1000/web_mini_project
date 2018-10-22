ffc0d7ab828883890ac254c526eeb24bbd7b4979
bhargav1000 <bhargavsagiraju@gmail.com>
2018-10-22 14:12:08 +0530

    changed textarea to input

--- form_handle.php
+++ form_handle.php
@@ -1,9 +1,27 @@
 <html>
 <body>
-	<?php echo $_POST["department"];?> 
- 	<?php echo $_POST["topic"];?>
- 	<?php echo $_POST["person"];?>
- 	<?php echo $_POST["phone"]; ?>
-	
-	</body>
+<?php
+// define variables and set to empty values
+$name = $email = $gender = $comment = $website = "";
+
+
+  $dept = test_input($_POST["department"]);
+  $topic = test_input($_POST["topic"]);
+  $person = test_input($_POST["person"]);
+  $description = test_input($_POST["description"]);
+
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+echo "Department: ".$description;
+echo "Talk On: ".$topic;
+echo "Resource Person: ".$person;
+echo "Phone: ".$phone;
+
+?>
+</body>
 </html>
