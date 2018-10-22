b13c40c5a0bcd09ea209a5f416718205220b6574
bhargav1000 <bhargavsagiraju@gmail.com>
2018-10-22 23:15:53 +0530

    php date

--- form_handle.php
+++ form_handle.php
@@ -6,6 +6,8 @@
 	echo $_POST['topic']."<br>";
 	echo $_POST['person']."<br>";
 	echo $_POST['phone']."<br>";
+	$dateof=date('d-m-Y', strtotime($_POST['dateof']));
+	echo $dateof."<br>";
 	echo $_POST['emailof']."<br>";
 	echo $_POST['audience']."<br>";
 	echo $_POST['year']."<br>";
